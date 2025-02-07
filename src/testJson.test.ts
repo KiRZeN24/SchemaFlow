6421d69d8c49ad740fca9c72cbff3e188a3fd0da
KiRZeN24 <carlosrufo92@gmail.com>
2025-02-07 16:27:12 +0100

    Add test for valid JSON

--- src/testJson.test.ts
+++ src/testJson.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { validateJson } from './validateJson'
 
 describe('validateJson', () => {
@@ -6,5 +7,12 @@ describe('validateJson', () => {
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe('Invalid JSON')
+  });
+
+  it('should return true', () => {
+    const data = '{"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
+    const resultJson = JSON.parse(validateJson(data))
+    expect(resultJson.isValid).toBe(true)
+    expect(resultJson.message).toBe('Valid JSON')
   });
 });
