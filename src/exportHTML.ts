b9b8f6a6811be9cba639dd5612b137f58587e87c
JeHidalgoch <jessica91quito@gmail.com>
2025-02-13 12:06:56 +0100

    Add the active with strong to the units (#6)

--- src/exportHTML.ts
+++ src/exportHTML.ts
@@ -12,9 +12,13 @@ interface Unit {
 export function exportHTML(data: Course) {
   let result = '';
   for (const unit of data.units) {
-    result += '<li>'+ unit.title +'</li>';
+    
+    if (unit.id === data.active) {
+      result += '<li><strong>'+ unit.title +'</strong></li>';
+    }else{
+      result += '<li>' + unit.title + '</li>';
+    }
   }
-
   const units ='<ul>'+ result + '</ul>';
   return `<h1>${data.course}</h1>
           ${units}`;
