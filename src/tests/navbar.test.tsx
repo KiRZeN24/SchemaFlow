81c4e4d2843075472855a3f763c6875836ff433f
darellanodev <darellano.aliaga@gmail.com>
2025-03-03 20:11:08 +0100

    Modify the test to focus exclusively on the Navbar component (#23)

--- src/tests/navbar.test.tsx
+++ src/tests/navbar.test.tsx
@@ -1,13 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, within } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import Navbar from "../navbar";
 
 describe("Navbar", () => {
   it("renders the title correctly", () => {
-    render(<Navbar />);
-    expect(screen.getByText("SchemaFlow")).toBeInTheDocument();
+    const { container } = render(<Navbar />);
+    expect(within(container).getByText("SchemaFlow")).toBeInTheDocument();
   });
-
-
 });
 
