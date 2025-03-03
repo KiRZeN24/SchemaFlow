732486dc6046349532200d9d76cfb3a38d24bb52
darellanodev <darellano.aliaga@gmail.com>
2025-03-03 20:10:32 +0100

    Use the Navbar into the App component (#23)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import SchemaForm from './schemaForm';
+import Navbar from './navbar';
 
 function App() {
   return (
-    <div>
-      <SchemaForm />
+    <div className="app-container">
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <SchemaForm />
+      </div>
     </div>
   );
 }
 
 export default App;
+
