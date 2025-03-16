9dd7ff1aadda990f75aecd29d6c47aa94e21fd9b
KiRZeN24 <carlosrufo92@gmail.com>
2025-03-16 15:23:45 +0100

    feat: Add About page component (#26)

---
+++ src/about.tsx
@@ -0,0 +1,34 @@
+import './about.css'
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-header">
+        <img src="./img_github_readme/logo.png" alt="SchemaFlow Logo" />
+        <h1>SchemaFlow</h1>
+        <p>flow your ideas</p>
+      </div>
+      <div className="about-content">
+        <p>
+          This project aims to improve the existing SchemaFlow, starting from
+          scratch and utilizing modern technologies such as React, TypeScript,
+          Vite, and Vitest.
+        </p>
+        <p className="about-author">
+          By <a href="https://github.com/KiRZeN24">KiRZeN24</a>
+        </p>
+        <p>
+          <a href="https://github.com/KiRZeN24/SchemaFlow">
+            View the GitHub repository
+          </a>
+        </p>
+        <p>Thanks to all the GitHub's contributors</p>
+      </div>
+      <div className="about-footer">
+        <span>Version: 0.1beta</span>
+      </div>
+    </div>
+  )
+}
+
+export default About
