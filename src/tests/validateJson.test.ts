636aab122b536610c66ab675fe068f1953aa0d21
darellanodev <darellano.aliaga@gmail.com>
2025-03-18 20:23:48 +0100

    test: Add schemaType property to validateJson tests and check for its presence (closes #33)

--- src/tests/validateJson.test.ts
+++ src/tests/validateJson.test.ts
@@ -11,7 +11,7 @@ describe('validateJson', () => {
 
   it('should return true', () => {
     const data =
-      '{"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
+      '{"schemaType":1,"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(true)
     expect(resultJson.message).toBe('Valid JSON')
@@ -24,9 +24,19 @@ describe('validateJson', () => {
     expect(resultJson.message).toBe('Invalid JSON')
   })
 
+  it('should return false if the schemaType is not present', () => {
+    const data =
+      '{"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
+    const resultJson = JSON.parse(validateJson(data))
+    expect(resultJson.isValid).toBe(false)
+    expect(resultJson.message).toBe(
+      'The schemaType property is missing in the JSON',
+    )
+  })
+
   it('should return false when there is not the course property in the JSON', () => {
     const data =
-      '{"units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
+      '{"schemaType":1,"units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe(
@@ -36,7 +46,7 @@ describe('validateJson', () => {
 
   it('should return false when there is not the active property in the JSON', () => {
     const data =
-      '{"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}]}'
+      '{"schemaType":1,"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}]}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe(
@@ -45,14 +55,14 @@ describe('validateJson', () => {
   })
 
   it('should return false when there is not the units property in the JSON', () => {
-    const data = '{"course":"ofimática","active":2}'
+    const data = '{"schemaType":1,"course":"ofimática","active":2}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe('The units property is missing in the JSON')
   })
 
   it('should return false when there are no units in the JSON', () => {
-    const data = '{"course":"ofimática","active":2,"units":[]}'
+    const data = '{"schemaType":1,"course":"ofimática","active":2,"units":[]}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe('The units is empty in the JSON')
@@ -60,7 +70,7 @@ describe('validateJson', () => {
 
   it('should return false when there is not id in a units', () => {
     const data =
-      '{"course":"ofimática","units":[{"title":"conceptos basicos"},{"title":"tablas de unidades"},{"title":"correcion ortografica"}],"active":2}'
+      '{"schemaType":1,"course":"ofimática","units":[{"title":"conceptos basicos"},{"title":"tablas de unidades"},{"title":"correcion ortografica"}],"active":2}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe(
@@ -70,7 +80,7 @@ describe('validateJson', () => {
 
   it('should return false if the id is no active', () => {
     const data =
-      '{"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":4}'
+      '{"schemaType":1,"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":4}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe('The id is not active in the JSON')
@@ -78,7 +88,7 @@ describe('validateJson', () => {
 
   it('should return false if does not have a title', () => {
     const data =
-      '{"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3}],"active":2}'
+      '{"schemaType":1,"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3}],"active":2}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe(
@@ -88,7 +98,7 @@ describe('validateJson', () => {
 
   it('should return false if the course title is empty', () => {
     const data =
-      '{"course":"","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
+      '{"schemaType":1,"course":"","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":2}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe('The course title is empty in the JSON')
@@ -96,9 +106,10 @@ describe('validateJson', () => {
 
   it('should return false if the active is not a number', () => {
     const data =
-      '{"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":"2"}'
+      '{"schemaType":1,"course":"ofimática","units":[{"id":1,"title":"conceptos basicos"},{"id":2,"title":"tablas de unidades"},{"id":3,"title":"correcion ortografica"}],"active":"2"}'
     const resultJson = JSON.parse(validateJson(data))
     expect(resultJson.isValid).toBe(false)
     expect(resultJson.message).toBe('The active is not a number in the JSON')
   })
 })
+
