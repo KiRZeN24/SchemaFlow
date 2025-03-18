1d05eaae9fc1d5965f0c398e3937955395e40a09
darellanodev <darellano.aliaga@gmail.com>
2025-03-18 20:17:45 +0100

    feat: Create the preview for the schema type 2 (#33)

--- src/schemaPreview.tsx
+++ src/schemaPreview.tsx
@@ -10,15 +10,34 @@ const SchemaPreview: React.FC<SchemaPreviewProps> = ({ formData }) => {
   return (
     <div className="schema-preview">
       <div className="title">{formData.course || 'Main Title'}</div>
-      {formData.units.map((unit) => (
-        <div
-          key={unit.id}
-          className={`subtitle ${
-            formData.active === unit.id ? 'selected' : ''
-          }`}>
-          {unit.title}
-        </div>
-      ))}
+      {formData.schemaType === 1 &&
+        formData.units.map((unit) => (
+          <div
+            key={unit.id}
+            className={`subtitle ${
+              formData.active === unit.id ? 'selected' : ''
+            }`}>
+            {unit.title}
+          </div>
+        ))}
+      {formData.schemaType === 2 && (
+        <ul
+          style={{ display: 'flex', padding: 0, margin: 0, listStyle: 'none' }}>
+          {formData.units.map((unit, index) => (
+            <li key={unit.id} style={{ display: 'flex', alignItems: 'center' }}>
+              <span
+                className={`subtitle ${
+                  formData.active === unit.id ? 'selected' : ''
+                }`}>
+                {unit.title}
+              </span>
+              {index < formData.units.length - 1 && (
+                <span style={{ margin: '0 10px' }}> ➡️ </span>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
