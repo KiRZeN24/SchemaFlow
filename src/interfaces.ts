e88638f93f9640f9ba18475509c76cd38ba1df33
KiRZeN24 <carlosrufo92@gmail.com>
2025-04-27 16:05:51 +0200

    Add interface SchemaFormPanel1 (#47)

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -23,6 +23,32 @@ export interface NavbarProps {
   onConfigurationClick: () => void
 }
 
+export interface SchemaFormPanel1Props {
+  onSchemaTypeChange: (value: string) => void
+  onLoadJSON: (value: string) => void
+  onClearAll: () => void
+  currentFormData: FormData
+}
+
+export interface SchemaFormPanel2Props {
+  formData: FormData
+  newSubtitle: string
+  setNewSubtitle: (value: string) => void
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  handleAddSubtitle: () => void
+  handleSetActive: (unitId: number) => void
+  handleRemoveSubtitle: (unitId: number) => void
+  handleMoveUp: (index: number) => void
+  handleMoveDown: (index: number) => void
+  startEditing: (index: number) => void
+  editingIndex: number | null
+  editText: string
+  setEditText: (value: string) => void
+  saveEdit: () => void
+  handleGenerateHTML: () => void
+  error: string
+}
+
 export interface SchemaFormPanel3Props {
   formData: FormData
   generatedHtml: string
